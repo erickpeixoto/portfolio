a608c1936cd856cf01016fae20a64d33643b4a6f
erickpeixoto <erickepeixoto@gmail.com>
2024-02-29 13:10:37 -0300

    feat: Navbar enhancements - WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
-import { unstable_noStore as noStore } from "next/cache";
-
 import { NavBar } from "@/app/components/navBar";
-import { api } from "@/trpc/server";
 
 export default async function Home() {
-  noStore();
-  const hi = await api.post.hello.query({
-    text: "from tRPC Erick's Boilerplate",
-  });
-
   return (
     <main className="flex flex-col min-h-screen items-center justify-between p-24 space-y-8 bg-bodyBg">
       <NavBar />
