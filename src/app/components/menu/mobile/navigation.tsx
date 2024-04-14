212a5f29812301098ace50353fd3c8b2cd463dbe
erickpeixoto <erickepeixoto@gmail.com>
2024-04-14 14:52:02 -0300

    feat: adjust mobile menu

--- src/app/components/menu/mobile/navigation.tsx
+++ src/app/components/menu/mobile/navigation.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { motion } from "framer-motion";
 import { MenuItem } from "./menu-items";
 import { cn } from "@/lib/utils";
+import { ThemeToggle } from "../../themeToggle";
 
 const variants = {
   open: {
@@ -16,7 +17,7 @@ export const Navigation = ({ isOpen }: { isOpen: boolean }) => (
   <motion.ul
     variants={variants}
     className={cn(
-      "flex flex-col w-full justify-center items-center p-8 pl-16 bg-white dark:bg-primary text-black dark:text-white",
+      "flex flex-col w-full justify-center items-center p-8 pl-16 bg-white z-40 dark:bg-primary text-black dark:text-white absolute",
       isOpen ? "block" : "hidden",
     )}
     initial={false}
@@ -26,6 +27,10 @@ export const Navigation = ({ isOpen }: { isOpen: boolean }) => (
     {itemIds.map((i) => (
       <MenuItem index={i} key={i} />
     ))}
+    <li className="flex gap-2 items-center">
+      Change Theme
+      <ThemeToggle />
+    </li>
   </motion.ul>
 );
 
