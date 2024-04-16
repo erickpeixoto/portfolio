0ba0c8ad1d86fa24e6291876d3ad25786f3be620
erickpeixoto <erickepeixoto@gmail.com>
2024-04-16 13:38:32 -0300

    feat: test production issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import Hero from "@/app/components/hero";
 import { Skills } from "@/app/components/skills";
 import Footer from "@/app/components/footer";
 
-export default async function TypewriterEffectDemo() {
+export default async function HomePage() {
   return (
     <>
-      <Hero />
       <Skills />
       <Footer />
     </>
