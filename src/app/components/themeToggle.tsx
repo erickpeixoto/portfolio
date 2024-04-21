be5d3e1cfd01f1c9b6a497558a39fc4237ceed00
erickpeixoto <erickepeixoto@gmail.com>
2024-04-21 17:58:07 -0300

    feat: adjust switch themes function

--- src/app/components/themeToggle.tsx
+++ src/app/components/themeToggle.tsx
@@ -1,37 +1,33 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { MdOutlineDarkMode, MdLightMode } from "react-icons/md";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/app/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/app/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+    <>
+      {theme === "dark" ? (
+        <Button
+          className="dark:text-white text-black dark:bg-identity md:dark:bg-black"
+          variant="outline"
+          onClick={() => setTheme("light")}
+        >
+          <MdLightMode className="w-6 h-6 text-white md:text-slate-500" />
         </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      ) : (
+        <Button
+          className="dark:text-white text-black"
+          variant="outline"
+          onClick={() => setTheme("dark")}
+        >
+          <MdOutlineDarkMode className="w-6 h-6 text-slate-500" />
+        </Button>
+      )}
+    </>
   );
 }
